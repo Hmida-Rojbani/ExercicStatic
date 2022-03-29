e023b3c3b779e9c669932f9585ce91a1f0e38c6f
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2022-03-29 14:06:03 +0100

    excercie

---
+++ src/AfficherCouleur.java
@@ -0,0 +1,22 @@
+public class AfficherCouleur {
+    public static void main(String[] args) {
+        FigureGeometrique f1, f2, f3, f4;
+        f1 = new FigureGeometrique("Rouge", "Noir");
+        f2 = new FigureGeometrique("Bleu", "Blanc");
+        f3 = new FigureGeometrique();
+        f4 = new FigureGeometrique("Violet", "Orange");
+ 
+        f2.changerCouleurContour("Noir");
+ 
+        System.out.println(f3.couleurSurface);
+        System.out.println(f3.couleurContour);
+ 
+        System.out.println(f2.couleurContour);
+ 
+        System.out.println(FigureGeometrique.nbFigures);
+        System.out.println(f2.nbFigures);
+ 
+        System.out.println(f1.nbFigContNoir);
+        System.out.println(f4.nbFigSurfBlanc);
+    }
+}
---
+++ src/FigureGeometrique.java
@@ -0,0 +1,9 @@
+ 
+public class FigureGeometrique {
+    String couleurContour = "Jaune";
+    String couleurSurface = "Blanc";
+ 
+    static int nbFigures = 0;
+    static int nbFigContNoir = 0;
+    static int nbFigSurfBlanc = 0;
+}
